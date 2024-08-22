fe59a3d88f32e852ee68ddf42c0a75a960a3d2b9
santosh ugale <santoshugale1991@gmail.com>
2024-08-22 21:48:51 +0530

    Added Swashbuckle.AspNetCore Nuget Package Added Swagger Support in Programs File

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 // Configure the HTTP request pipeline.
 
 var summaries = new[]
